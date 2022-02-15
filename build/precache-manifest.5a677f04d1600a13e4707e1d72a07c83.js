4e56932cebae804f4e632174d9fb42bfb64310b1
Brian Walker <bw01496@gmail.com>
2022-02-14 21:27:05 -0500

    first commit from copy

---
+++ build/precache-manifest.5a677f04d1600a13e4707e1d72a07c83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2acf95dbc81e5e324196ee90ff6c74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a74bca145e178603c9",
+    "url": "/static/js/2.46712fcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.46712fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bca850759aa9ad83bee",
+    "url": "/static/js/main.320e37db.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  }
+]);
